4fd72f187a9474dd9f94dd934cf5fa3d94535972
Sariah-dotcom <74076170+Sariah-dotcom@users.noreply.github.com>
2025-06-03 15:12:39 -0400

    Add files via upload

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,18 +1,34 @@
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 import React, { useRef } from 'react';
-import { GoArrowUpRight } from "react-icons/go";
-import { Link } from 'react-router-dom';
+import { IoIosArrowForward } from "react-icons/io";
 
 export default function Hero() {
   const heroText = useRef();
-  const heroImg = useRef();
-  const heroBg = useRef();
+  const heroBtns = useRef();
   const heroUnderline = useRef();
+  const marqueeRef = useRef();
 
+  // Carousel
+  function pauseCarousel() {
+    
+  }
 
+  // Slides array
+  const images = [
+    '/posters/postbox-rental.png',
+    '/heroImgs/hero-2.jpg',
+    '/heroImgs/hero-3.jpg',
+    '/posters/moneygram.png',
+    '/heroImgs/hero-5.jpg',
+   '/heroImgs/hero-3.jpg',
+    '/posters/usmailbox.png',
+    '/heroImgs/hero-5.jpg',
+  ];
+  const repeatedImages = [...images, ...images]; 
+
+  // Hero Animations
   useGSAP(() => {
-
     const path = heroUnderline.current.querySelector('path');
     const length = path.getTotalLength();
 
@@ -24,10 +40,10 @@ export default function Hero() {
 
     gsap.to(path, {
       strokeDashoffset: 0,
-      opacity:1,
+      opacity: 1,
       duration: 1,
       ease: "power3",
-      delay: 1 // adjust to sync with text animation
+      delay: 1.6
     });
 
     gsap.fromTo(
@@ -38,69 +54,102 @@ export default function Hero() {
       },
       {
         opacity: 1,
+        delay: 0.8,
         y: 0,
         duration: 1.3,
-        stagger: 0.2,
+        stagger: 0.4,
         ease: "power3"
       }
     );
 
-    gsap.fromTo(heroImg.current.querySelectorAll('.hero-imgs'),
+    gsap.fromTo(
+      heroBtns.current.querySelectorAll('.hero-btns'),
       {
-        y: -300,
-        opacity: 0
-      }, 
+        y: 100,
+        opacity: 0,
+      },
       {
-        y: 0,
         opacity: 1,
-        duration: 1.4,
-        stagger: 0.3,
+        delay: 1.4,
+        y: 0,
+        duration: 1,
+        stagger: 0.5,
+        ease: "power3.out"
+      }
+    );
+
+    gsap.fromTo(
+      marqueeRef.current,
+      {
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+        delay: 1.8,
+        duration: 1,
         ease: "power3"
       }
     );
   }, []);
 
   return (
-    <section className='p-3 md:p-5 flex items-center lg:h-[90vh]'>
-      <div className='p-3 w-full flex flex-col gap-10 lg:flex-row lg:items-center lg:h-[80vh] lg:px-50 lg:py-0 lg:gap-5  rounded-[1rem] hero-bg' ref={heroBg}>
-        {/* Hero text */}
-        <div className="left h-1/2 flex flex-col gap-3 pt-20 lg:pt-0 lg:w-1/2" ref={heroText}>
-          <h1 className='text-6xl font-semibold lg:text-8xl hero-line text-[#001A6E]'>
-            Mail Made <br />
-            <span className="relative inline-block">
-              Simple
-              <svg
-                className="absolute left-0 md:-bottom-8 w-full h-auto"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 310 40"
-                ref={heroUnderline}
-              >
-                <path
-                  d="M4.99805 20.9998C65.6267 17.4649 126.268 13.845 187.208 12.8887C226.483 12.2723 265.751 13.2796 304.998 13.9998"
-                  fill="none"
-                  stroke="#A0C2F4"
-                  strokeWidth="5"
-                  strokeLinecap="round"
-                />
-              </svg>
-            </span>
-          </h1>
+    <section className='flex flex-col gap-10 items-center px-5 overflow-x-hidden '>
+      <div className="text-center mt-15 md:mt-30 flex flex-col items-center gap-10" ref={heroText}>
+        <h1 className='hero-line text-5xl text-center flex flex-col lg:flex-row lg:gap-5 font-semibold text-(--color-navy) lg:text-8xl'>
+          Mail Made
+          <span className="relative inline-block">
+            Simple
+            <svg
+              className="absolute -z-1 -bottom-6 lg:-bottom-8 left-0 w-full h-auto"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 310 40"
+              ref={heroUnderline}
+            >
+              <path
+                d="M4.99805 20.9998C65.6267 17.4649 126.268 13.845 187.208 12.8887C226.483 12.2723 265.751 13.2796 304.998 13.9998"
+                fill="none"
+                stroke="#A0C2F4"
+                strokeWidth="5"
+                strokeLinecap="round"
+              />
+            </svg>
+          </span>
+        </h1>
+        <p className='text-center text-sm lg:text-lg text-gray-600 lg:w-[40%] hero-line'>
+          SVG Post is here to keep you connected. Whether you're sending a letter across town or a package across the country, our trusted service combines tradition with modern convenience—delivering reliability, speed, and care every step of the way.
+        </p>
 
-          <h2 className='font-medium lg:text-xl xl:text-2xl hero-line text-[#001A6E] mt-4'>Track, Send, and Receive with Confidence</h2>
-          <p className='text-gray-500 mt-3 lg:w-[80%] hero-line'>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque rem incidunt provident possimus placeat? Sint nisi dicta nulla illo, aliquid ab.
-          </p>
-          <Link to="/" className='mt-2 group flex items-center gap-1 text-lg underline text-[#001A6E] font-semibold hover:text-[#A0C2F4] hero-line'>
-            Track a Package <GoArrowUpRight className='group-hover:ml-1' />
-          </Link>
-        </div>
-
-        {/* Hero Images */}
-        <div className="right w-full lg:h-[80%] lg:w-1/2 flex justify-center items-center mt-10" ref={heroImg}>
-          <img src="/blob.png" className='h-90 md:h-auto hero-imgs' alt="" />
-          <img src="/hero-img-1.png" className='absolute h-100 md:h-auto hero-imgs' alt="" />
+        <div className='flex gap-3 lg:gap-5' ref={heroBtns}>
+          <button
+            className='hero-btns text-sm flex gap-1 items-center justify-center text-white p-3 rounded-full lg:pl-4 lg:pr-3'
+            style={{ backgroundColor: 'var(--color-navy)' }}
+          >
+            Track a Package <IoIosArrowForward />
+          </button>
+          <button className='hero-btns text-sm flex gap-1 items-center justify-center text-gray-700 bg-gray-300 lg:pl-4 lg:pr-3 p-3 rounded-full'>
+            View our Services <IoIosArrowForward />
+          </button>
         </div>
       </div>
+
+      
+
+      {/* Carousel */}
+      <div>
+        <div className="marquee-wrapper mt-5 lg:mt-50" ref={marqueeRef}>
+            <div className="marquee-track" onMouseEnter={pauseCarousel()}>
+              {repeatedImages.map((src, index) => (
+                <div className="marquee-item h-[20rem] w-[15rem] lg:w-[30rem] lg:h-[40rem]" key={index}>
+                  <img src={src} alt={`Slide ${index + 1}`} className="marquee-image" />
+                </div>
+              ))}
+            </div>
+        </div>
+    <div className=""></div>
+      </div>
+      <div></div>
+      
+      
     </section>
   );
 }
