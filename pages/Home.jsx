21fa8e0bd9e60ec291bb26a907f0e20c8c68240e
Sariah-dotcom <74076170+Sariah-dotcom@users.noreply.github.com>
2025-06-06 14:30:41 -0400

    Add files via upload

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,24 +1,23 @@
-import React from 'react'
+import { useRef } from 'react'
 import Hero from '../components/Hero'
 import Services from '../components/Services'
 import Locations from '../components/Locations'
-import About from '../components/About'
 import Faqs from '../components/Faqs'
 import Promotions from '../components/Promotions'
 
-export default function Home() {
-  return (
-   <div className="flex flex-col gap-[5rem]">
-    <Hero />
-    <div className="flex items-center justify-center">
-      <About />
 
-    </div>
-      <Services />
-      <Locations />
-      <Faqs />
-      <Promotions />
-      <div className="mb-[8rem]"></div>
+
+export default function Home() {
+
+  return (
+   <div className="flex flex-col gap-[5rem] lg:gap-[15rem]">
+    <Hero />
+    <Services />
+    <Locations />
+    <Promotions />
+    {/* <Faqs />
+    <Promotions /> */}
+    <div className="mb-[8rem]"></div>
    </div>
   )
 }
